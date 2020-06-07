66c33531f12bb037c6d7372d7457f9ee39fbc781
Melvin Bender <melvin.bender@tum.de>
2020-06-07 17:22:15 +0200

    updates publish script and latency pla

--- plans/publish.bash
+++ plans/publish.bash
@@ -3,6 +3,10 @@
 # Script for resource usages tests of mosquitto sever
 # first argument is port number
 # second argument is number of topics to subscribe to
+# exit on error
+set -e
+# log every command
+set -x
 local="/media/sf_shared/commands"
 a="$1"
 
