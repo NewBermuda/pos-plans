5c450f25f32aa615ef8bd5a8173b5ce6d453a72e
Melvin Bender <melvin.bender@tum.de>
2020-08-05 14:19:40 +0200

    updatey python script

--- client.py
+++ client.py
@@ -1,12 +1,11 @@
+
 import paho.mqtt.client as mqtt
+import sys
+
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("test")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
@@ -18,8 +17,12 @@ client.on_message = on_message
 
 client.connect("172.16.1.1", 1999, 60)
 
+n = int(sys.argv[1])
+for i in range(n):
+	client.publish('test', payload='Test1', qos=0, retain=False)
+
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
 # Other loop*() functions are available that give a threaded interface and a
 # manual interface.
-client.loop_forever()
+client.disconnect()
